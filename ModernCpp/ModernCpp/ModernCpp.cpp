0419fefcd47721c6eecca16184659aea66a5a542
rpotluri <potluri.ravikumar@gmail.com>
2020-01-02 14:33:10 +0100

    Some more common features of C++ 11

--- ModernCpp/ModernCpp/ModernCpp.cpp
+++ ModernCpp/ModernCpp/ModernCpp.cpp
@@ -1,12 +1,33 @@
 #include "pch.h"
 #include <iostream>
+#include <initializer_list>
 
+class Test {
+	//in class member initialization; we do not need a default constructor for initialization
+	int m_Val{}; // int m_Val = 0;
+public:
+	//constructor with variables initialized
+	/*Test() :m_Val{0} {
+
+	}*/
+	//telling compiler to synthesize default constructor
+	Test() = default;
+	//construtor with parameter initialization
+	Test(int x) :m_Val{ x } {
+
+	}
+};
 void Print(std::initializer_list<int> list) {
 	// Auto has to be initialize variable with function return
 	auto it = list.begin();
-	while (it != list.end()) {
+	/*while (it != list.end()) {
 		std::cout << *it << " ";
 		++it;
+	}*/
+	//range based for loop
+	//for (int x : list) {
+	for (const auto &x : list) {
+		std::cout << x << ' ';
 	}
 }
 
