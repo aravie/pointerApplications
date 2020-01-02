fa0864d24dfdd744a74c3832419daa17646bc32d
rpotluri <potluri.ravikumar@gmail.com>
2020-01-02 14:11:38 +0100

    auto to initialize variable and pointer

--- ModernCpp/ModernCpp/ModernCpp.cpp
+++ ModernCpp/ModernCpp/ModernCpp.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 
 void Print(std::initializer_list<int> list) {
-	std::initializer_list<int>::iterator it = list.begin();
+	// Auto has to be initialize variable with function return
+	auto it = list.begin();
 	while (it != list.end()) {
 		std::cout << *it << " ";
 		++it;
@@ -15,9 +16,10 @@ int main()
 	//Uniform intialization
 	int i{ 0 };
 	int *ptr{};
+	//auto to initialize pointer
+	auto *ptr2 = ptr;
 	int arr[5] = { 1,2,3 };
 
 	std::initializer_list<int> list{ 1,2,3,4 };
 	Print(list);
 }
-
