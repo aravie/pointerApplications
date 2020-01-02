6cd3557b0f50a936cf284647993b3979b9498411
rpotluri <potluri.ravikumar@gmail.com>
2020-01-02 21:08:20 +0100

    passing array as a parameter to function

--- StaticArrays/StaticArrays/main.cpp
+++ StaticArrays/StaticArrays/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+//passing array as pointer to function, along with size details to access it
+void Print(int *ptr, int size) {
+	for (int i = 0; i < size; ++i) {
+		std::cout << ptr[i] << ' ';
+		//std::cout << *(ptr + i) << ' ';
+	}
+}
+
 int main() {
 	int arr1[5];
 	int arr2[5]{};
@@ -10,6 +18,8 @@ int main() {
 
 	int *p = arr3;
 	*(p + 2) = 800;
-
+	//Print(arr3, sizeof(arr3)/sizeof(int));
+	//undefined behavior if wrong size of array is passed as a parameter to function
+	Print(arr3, 10);
 	return 0;
 }
