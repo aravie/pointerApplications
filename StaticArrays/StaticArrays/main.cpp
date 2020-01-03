79a393f8e08e62adc602eeccde29c700f8ca6aba
rpotluri <potluri.ravikumar@gmail.com>
2020-01-03 10:12:22 +0100

    standard array in c++, has function for bound checking

--- StaticArrays/StaticArrays/main.cpp
+++ StaticArrays/StaticArrays/main.cpp
@@ -1,59 +1,29 @@
 #include <iostream>
 #include <algorithm>
+#include <array>
 
-//passing array as pointer to function, along with size details to access it
-void Print(int *ptr, int size) {
-	for (int i = 0; i < size; ++i) {
-		std::cout << ptr[i] << ' ';
-		//std::cout << *(ptr + i) << ' ';
-	}
-}
 
-//creating a function using template
-template<typename T, int size>
-void Print(T(&ref)[size]) {
-	for (int i = 0; i < size; ++i) {
-		std::cout << ref[i] << ' ';
-		//std::cout << *(ptr + i) << ' ';
-	}
-}
-
-template<typename T, int size>
-T * Begin(T(&ref)[size]) {
-	return ref;
-}
-
-template<typename T, int size>
-T* End(T(&ref)[size]) {
-	return (ref + size);
+void Print(std::array<int, 5> &arr) {
+	std::sort(arr.begin(), arr.end());
+	//for (int i = 0; i < arr.size(); ++i) {
+	//	std::cout << arr[i] << ' ';
+	//}
+	//range based for loop 
+	//for (auto x : arr)
+	//for array of user defined objects
+	for(const auto &x : arr)
+		std::cout << x << ' ';
 }
 
 int main() {
-	int arr1[5];
-	int arr2[5]{};
-	int arr3[5]{ 1,4,5,3,2 };
+	//initialize a standard array
+	std::array<int, 5> arr{ 4,2,3,1,5 };
+	arr.size();
+	arr[0];
+	//accessing the elements with at has bounds checking
+	arr.at(0);
 
-	//decomposing array to pointer
-	arr3[0] = 100;
-	//creating reference to array by specifying size of array
-	int(&ref)[5] = arr3;
-
-	//int *p = arr3;
-	//int *end = arr3 + 5;
-	//int *p = Begin(arr3);
-	//int *end = End(arr3);
-	int *p = std::begin(arr3);
-	int *end = std::end(arr3);
-	//dereferencing the array
-	*(p + 2) = 800;
-	p[2] = 700;
-	//std::sort(p, end);
-	std::sort(std::begin(arr3), std::end(arr3));
-	//Print(arr3, sizeof(arr3)/sizeof(int));
-	//undefined behavior if wrong size of array is passed as a parameter to function
-	//Print(arr3, 10);
-	//passing array by just specifying name of the array
-	Print(arr3);
+	Print(arr);
 
 	return 0;
 }
